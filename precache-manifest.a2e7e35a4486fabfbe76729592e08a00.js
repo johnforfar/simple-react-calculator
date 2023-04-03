f916806eba01befd3e3bb6a06a1e73431b233064
SOL X <john.forfar@gmail.com>
2023-04-03 13:33:52 +1000

    Updates

---
+++ precache-manifest.a2e7e35a4486fabfbe76729592e08a00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5d951d0423710b4ae5e",
+    "url": "/static/css/main.7e76fe49.chunk.css"
+  },
+  {
+    "revision": "b5d951d0423710b4ae5e",
+    "url": "/static/js/main.b5d951d0.chunk.js"
+  },
+  {
+    "revision": "8bcc787db593634d27ed",
+    "url": "/static/js/1.8bcc787d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a86e87995589d06e997b014bd95c3d04",
+    "url": "/index.html"
+  }
+];
